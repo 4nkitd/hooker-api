696ec2a7bb16a9ae096e46ff88d6e1535615a70f
unknown <Ankit>
2024-11-10 19:59:12 +0530

    new request 9

--- src/index.ts
+++ src/index.ts
@@ -190,8 +190,47 @@ async function handleRequestDetails(request: Request, env: Env): Promise<Respons
 	}
 }
 
+async function handleOptions(request) {
+	if (
+		request.headers.get("Origin") !== null &&
+		request.headers.get("Access-Control-Request-Method") !== null &&
+		request.headers.get("Access-Control-Request-Headers") !== null
+	) {
+
+		const corsHeaders = {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+			"Access-Control-Max-Age": "86400",
+		  };
+		// Handle CORS preflight requests.
+		return new Response(null, {
+			headers: {
+				...corsHeaders,
+				"Access-Control-Allow-Headers": request.headers.get(
+					"Access-Control-Request-Headers",
+				),
+			},
+		});
+	} else {
+		// Handle standard OPTIONS request.
+		return new Response(null, {
+			headers: {
+				Allow: "GET, HEAD, POST, OPTIONS",
+			},
+		});
+	}
+}
+
+
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+
+		if (request.method === "OPTIONS") {
+			// Handle CORS preflight requests
+			return handleOptions(request);
+		  }
+
 		const url = new URL(request.url);
 		if (url.pathname === '/new/hook') {
 			return handleNewHookRequest(request, env);
