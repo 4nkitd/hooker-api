1464ed98fc192990b5bb195781e5c65130654ce4
unknown <Ankit>
2024-11-10 18:56:21 +0530

    new request

--- schema.sql
+++ schema.sql
@@ -1,21 +1,19 @@
 DROP TABLE IF EXISTS `requests`;
 CREATE TABLE `requests` (
-  `id` bigint(20) NOT NULL,
-  `uuid` int(11) NOT NULL,
+  `uuid` tinytext NOT NULL,
+  `webhook_id` tinytext NOT NULL,
   `body` text NOT NULL,
   `headers` text NOT NULL,
   `ip` tinytext NOT NULL,
   `method` int(11) NOT NULL,
   `is_cron` tinyint(4) NOT NULL,
   `updated_at` timestamp NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`id`)
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 );
 
 
 DROP TABLE IF EXISTS `webhooks`;
 CREATE TABLE `webhooks` (
-  `id` int(11) NOT NULL,
   `uuid` tinytext NOT NULL,
   `user_id` bigint(20) DEFAULT NULL,
   `description` text DEFAULT NULL,
@@ -25,6 +23,5 @@ CREATE TABLE `webhooks` (
   `custom_js` text  NULL,
   `salt` text NULL,
   `updated_at` timestamp NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ,
-  PRIMARY KEY (`id`)
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ;
--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ async function handleNewHookRequest(request: Request, env: Env): Promise<Respons
 		const snowflakeId = uuidv4();
 		const now_timestamp = new Date().toISOString();
 		const insertQuery = ` INSERT INTO webhooks
-			(uuid, user_id, description, active, total_req_count, is_redirect, custom_js, salt, updated_at, created_at)
+			( uuid, user_id, description, active, total_req_count, is_redirect, custom_js, salt, updated_at, created_at)
 			VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
 		`;
 		await client.prepare(insertQuery).bind(snowflakeId, null, null, 1, 0, 0, null, null, now_timestamp, now_timestamp).run();
@@ -43,11 +43,69 @@ async function handleNewHookRequest(request: Request, env: Env): Promise<Respons
 
 }
 
+async function handleNewRequest(request: Request, env: Env): Promise<Response> {
+	if (request.method !== 'POST') {
+		return new Response('Method Not Allowed', { status: 405 });
+	}
+
+	const client = env.DB;
+
+	try {
+
+		let hookId = request.url.split('/r/').pop();
+
+		if (!hookId) {
+			return new Response(JSON.stringify({
+				"status": false,
+				"error": "Hook ID not found",
+			}), { status: 400 });
+		}
+
+		const selectQuery = `SELECT * FROM webhooks WHERE uuid = ?`;
+		const webhookData = await client.prepare(selectQuery).bind(hookId).first();
+
+		if (!webhookData) {
+			return new Response(JSON.stringify({
+				"status": false,
+				"error": "Webhook not found",
+			}), { status: 404 });
+		}
+
+		let requestbody = await request.json();
+		let headers = JSON.stringify(request.headers);
+		let method = request.method;
+		let ip = request.headers.get('cf-connecting-ip');
+
+		let requestID = uuidv4();
+
+		const now_timestamp = new Date().toISOString();
+		const insertQuery = `INSERT INTO requests
+			(uuid,webhook_id, body, headers, ip, method, is_cron, updated_at, created_at)
+			VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+		`;
+		await client.prepare(insertQuery).bind(requestID, hookId, JSON.stringify(requestbody), headers, ip, method, 0, now_timestamp, now_timestamp).run();
+		return new Response(JSON.stringify({ status: true, id: requestID, webhhok: webhookData }), {
+			headers: { 'Content-Type': 'application/json' },
+		});
+	} catch (error) {
+		return new Response(JSON.stringify({
+			"status": false,
+			"error": "Error while creating new hook",
+			"log": error.message,
+		}), { status: 500 });
+	}
+
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
 		if (url.pathname === '/new/hook') {
 			return handleNewHookRequest(request, env);
+		}
+		const rPathMatch = url.pathname.match(/^\/r\/(.+)/);
+		if (rPathMatch) {
+			return handleNewRequest(request, env);
 		}
 		return new Response('Hello World!');
 	},
